14d6e048c5f0d76ef47275d1cde5c1bd6b551647
Abdul-Rahman Gamal <abdulrahmangamal4u@outlook.com>
2026-06-30 18:27:27 +0300

    Reapply "worked on cart, checkout and confirmation"
    This reverts commit 38b1da0c07bdc68226fb8ba22857aef1c4aa46db.

--- CartFlow.Services/Services/AccountService.cs
+++ CartFlow.Services/Services/AccountService.cs
@@ -1,7 +1,28 @@
 using CartFlow.Services.Interfaces;
+using CartFlow.Data.Data;
+using CartFlow.Data.Entities;
+using CartFlow.Data.Enums;
+using Microsoft.EntityFrameworkCore;
 
 namespace CartFlow.Services.Services;
 
-public class AccountService : IAccountService {
-    // Auth methods will be added when Identity is wired up
+public class AccountService(AppDbContext context) : IAccountService {
+    public async Task<User?> SignInAsync(string email, string password) {
+        return await context.Users.FirstOrDefaultAsync(u => u.Email == email && u.Password == password);
+    }
+    public async Task<User?> SignUpAsync(string firstname, string lastName, string email, string password) {
+        var user = new User {
+            FirstName = firstname,
+            LastName = lastName,
+            Email = email,
+            Password = password,
+            UserRole = Role.CUSTOMER
+        };
+
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
+        return user;
+    }
+
+
 }
